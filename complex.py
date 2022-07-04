e5b437ada4beaaf3d5423f38bafdf803863bd6d4
htoh702 <htoh702@gmail.com>
2022-07-04 16:36:30 +0900

    base complex class added

--- complex.py
+++ complex.py
@@ -10,11 +10,6 @@ class Complex:
 
     def __str__(self):
         return str(self.re)+"+"+str(self.im)+"im"
-    
-    
-    def multiplication(self):
-        print(str(self.re) +"x"+ str(self.im) +"="+str(self.re*self.im))
-        
+
 c1=Complex(1,2)
 print(c1)
-c1.multiplication()
