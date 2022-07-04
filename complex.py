83c0271b0fefee83ef8fe88c38042d3a39320eb6
imlay128 <artisboom@naver.com>
2022-07-04 17:02:33 +0900

    substraction modified

--- complex.py
+++ complex.py
@@ -9,10 +9,12 @@ class Complex:
         self.im=im
 
     def __str__(self):
-        return str(self.re)+"+"+str(self.im)+"im"
+        return str(self.re)+"+"+str(self.im)+"i"
         
     def substraction(self, x):
         return Complex(self.re - x.re, self.im - x.im)
 
 c1=Complex(1,2)
-print(c1)
+c2=Complex(2,3)
+print(c1.substraction(c2))
+
