af7856e618890e87012db3c38fc2143178441461
백동렬 <BamBuStiK+dbstickman@gmail.com>
2022-07-04 16:23:26 +0900

    base complex class added

---
+++ complex.py
@@ -0,0 +1,15 @@
+"""
+백동렬:maintainer
+허규:addition,subtraction
+오현택:multiplication
+"""
+class Complex:
+    def __init__(self, re=0, im=0):
+        self.re=re
+        self.im=im
+
+    def __str__(self):
+        return str(self.re)+"+"+str(self.im)+"im"
+
+c1=Complex(1,2)
+print(c1)
