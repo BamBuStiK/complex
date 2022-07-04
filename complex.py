03530a833998c642461a73ed88f2a63efd42fb3d
htoh702 <htoh702@gmail.com>
2022-07-04 16:49:30 +0900

    multiplication

--- complex.py
+++ complex.py
@@ -11,5 +11,14 @@ class Complex:
     def __str__(self):
         return str(self.re)+"+"+str(self.im)+"im"
 
+    def multiple(self, c1):
+        c = Complex()
+        c.re = self.re*c1.re
+        c.im = self.im*c1.im
+        return c
+
 c1=Complex(1,2)
 print(c1)
+
+c2=Complex(2,3)
+print(c2.multiple(c1))
