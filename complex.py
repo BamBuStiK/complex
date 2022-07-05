0a194943e658a0e15c2bc2e7dddd6ebacc83e4c2
imlay128 <artisboom@naver.com>
2022-07-05 15:44:29 +0900

    op_sub branch added

--- complex.py
+++ complex.py
@@ -11,6 +11,9 @@ class Complex:
     def __str__(self):
         return str(self.re)+"+"+str(self.im)+"i"
         
+    def addition(self, x):
+        return Complex(self.re + x.re, self.im + x.im)
+        
     def substraction(self, x):
         return Complex(self.re - x.re, self.im - x.im)
 
@@ -19,13 +22,12 @@ class Complex:
         c.re = self.re*c1.re
         c.im = self.im*c1.im
         return c
-
+    
+    def __sub__(self, c):
+        return self.substraction(c)
+        
 c1=Complex(1,2)
 c2=Complex(2,3)
 print(c1.addition(c2))
-print(c1)
-
-c2=Complex(2,3)
 print(c2.multiple(c1))
-c2=Complex(2,3)
-print(c1.substraction(c2))
+print(c2 - c1)
