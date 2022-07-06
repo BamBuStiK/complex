04a186141f452568ec40f8486cfb4ecbdf2a120f
oh hyeun taek <79206548+htoh702@users.noreply.github.com>
2022-07-06 15:35:35 +0900

    Update complex.py

--- complex.py
+++ complex.py
@@ -29,16 +29,18 @@ class Complex:
         c.im = self.re*c1.im+self.im*c1.re
         return c
         
-    def __add__(self,c):
+    def __add__(self,c):    # self + c
         return self.addition(c)
-    def __sub__(self,c):
+    
+    def __sub__(self,c):    # self - c
         return self.subtraction(c)
-    def __mul__(self,c):
+    
+    def __mul__(self,c):    # self * c
         return self.multiple(c)
     
 c1=Complex(1,2)
 print(c1)
 c2=Complex(2,3)
-print(c1+c2)
-print(c1+c2)
-print(c1+c2)
+print(c1+c2)    # c1 + c2
+print(c1-c2)    # c1 - c2
+print(c1*c2)    # c1 * c2
