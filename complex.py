6bccaab2eb7d9271e295ef73ac913b9686b81c6b
BamBuStiK <70628913+BamBuStiK@users.noreply.github.com>
2022-07-06 15:18:35 +0900

    Update complex.py

--- complex.py
+++ complex.py
@@ -11,8 +11,13 @@ class Complex:
     def __str__(self):
         return str(self.re)+"+"+str(self.im)+"i"
 
+    def addition(self,c1):
+        c=Complex()
+        c.re=self.re+c1.re
+        c.im=self.im+c1.im
+        return c
 
-    def substraction(self, x):
+    def substraction(self, c1):
         c=Complex()
         c.re=self.re - c1.re
         c.im=self.im-c1.im
@@ -23,3 +28,10 @@ class Complex:
         c.re = self.re*c1.re-self.im*c1.im
         c.im = self.re*c1.im+self.im*c1.re
         return c
+        
+c1=Complex(1,2)
+print(c1)
+c2=Complex(2,3)
+print(c1.add(c2))
+print(c2.subtract(c1))
+print(c1.multiply(c2))
